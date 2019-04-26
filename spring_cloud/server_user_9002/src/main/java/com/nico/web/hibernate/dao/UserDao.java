7ef899a0a3b1f68a15b2edb1411a26e9dcf0b3f4
liguanping <liguanping@e-future.com.cn>
2019-04-26 09:20:13 +0800

    2019-4-26 9:20

--- spring_cloud/server_user_9002/src/main/java/com/nico/web/hibernate/dao/UserDao.java
+++ spring_cloud/server_user_9002/src/main/java/com/nico/web/hibernate/dao/UserDao.java
@@ -9,24 +9,29 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.nico.web.hibernate.entity.User;
 
-public interface  UserDao extends BaseJpaDao<User, Long> {
-	//查询用户名称包含username字符串的用户对象-
-    List<User> findByUsernameContaining(String username);
+public interface UserDao extends BaseJpaDao<User, Long> {
+	// 查询用户名称包含username字符串的用户对象-
+	List<User> findByUsernameContaining(String username);
 
-    //获得单个用户对象，根据username和pwd的字段匹配
-    User getByUsernameIsAndUserpwdIs(String username,String pwd);
+	// 获得单个用户对象，根据username和pwd的字段匹配
+	User getByUsernameIsAndUserpwdIs(String username, String pwd);
 
-    //精确匹配username的用户对象
-    User getByUsernameIs(String username);
-    
-    //上面的JPA自动根据find  get  匹配关键字和后面的条件进行查询，以官方规则为准
-    //下面的是手动注入SQL查询
-    @Query("select w from User w where w.loginName = :loginName")
-    User searchUserWeibo(@Param("loginName") String loginName);
-    
-    @Modifying
-    @Transactional(readOnly = false)
-    @Query("update User w set w.loginName = :loginName where w = :user")
-    int  setUserById(@Param("loginName") String loginName,@Param("user")User user);
-    
+	// 精确匹配username的用户对象
+	User getByUsernameIs(String username);
+
+	// 上面的JPA自动根据find get 匹配关键字和后面的条件进行查询，以官方规则为准
+	// 下面的是手动注入SQL查询
+	@Query("select w from User w where w.loginName = :loginName")
+	User searchUserWeibo(@Param("loginName") String loginName);
+
+	@Modifying
+	@Transactional(readOnly = false)
+	@Query("update User w set w.loginName = :loginName where w = :user")
+	int setUserById(@Param("loginName") String loginName, @Param("user") User user);
+
+	@Modifying
+	@Transactional
+	@Query("delete w from User w where w = :user ")
+	int delUserById(@Param("loginName") String loginName, @Param("user") User user);
+
 }
